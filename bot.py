ec986b0bb685466c8e5d21ee1f740ff3de3ebca6
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-09-26 18:28:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,9 +22,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 logger = logging.getLogger(__name__)
 
 
-APP_ID = os.environ.get("APP_ID", None)
-APP_HASH = os.environ.get("APP_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+APP_ID = "4091096"
+APP_HASH = "6bb0682b4af56456201c3b9d8b99c94a"
+BOT_TOKEN = "2011095989:AAFNXWUHkhj1DKK6Dh9w8S79lvrjZk0Adrs"
 TMP_DOWNLOAD_DIRECTORY = os.environ.get(
     "TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 MONGO_DB = os.environ.get("MONGO_DB", None)
